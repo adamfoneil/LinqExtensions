a783589a274664851327b04ae52b8bf43bc5d6e2
Adam O'Neil <adamosoftware@gmail.com>
2020-08-14 21:10:40 -0400

    Add project files.

---
+++ LinqExtensions/IEnumerableExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AO.Linq.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static ILookup<int, T> Paginate<T>(this IEnumerable<T> items, int itemsPerPage)
+        {
+            var paged = items.Select((item, index) => new
+            {
+                Item = item,
+                Page = getPage(index + 1)
+            });
+
+            return paged.ToLookup(item => item.Page, item => item.Item);
+
+            int getPage(int index)
+            {
+                int result = index / itemsPerPage;
+                int leftover = (index % itemsPerPage > 0) ? 1 : 0;
+                return result + leftover;
+            }
+        }
+
+        public static ILookup<int, T> Partition<T>(this IEnumerable<T> items, int partitionCount)
+        {
+            int count = items.Count();
+            int partitionaSize = count / partitionCount;
+
+            // need to know if the partitions are unequal
+            bool isOdd = (count % partitionCount) != 0;
+
+            // figure out the start and end of each partition (bucket)
+            var partitions = Enumerable.Range(0, partitionCount).Select(p =>
+            {
+                int start = (p * partitionaSize) + 1;
+                int end = (p + 1) * partitionaSize;
+                // if we're on the last partition and the partitions are odd, we need to pad the end
+                int padding = ((p + 1 == partitionCount) && isOdd) ? count - end : 0;
+                int realEnd = end + padding;
+
+                return new
+                {
+                    Index = p,
+                    Range = Enumerable.Range(start, realEnd - start + 1).ToArray()
+                };
+            })
+            // flatten these ranges into a single sequence
+            .SelectMany(partition => partition.Range, (partition, value) => new
+            {
+                Partition = partition.Index,
+                Value = value
+            })
+            // key the partition values to each array index
+            .ToDictionary(row => row.Value, row => row.Partition);
+
+            // group all the items by their related partition
+            return items.Select((item, index) => new
+            {
+                Item = item,
+                Partition = partitions[index + 1]
+            }).ToLookup(item => item.Partition, item => item.Item);
+        }
+    }
+}
---
+++ Testing/LinqTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using AO.Linq.Extensions;
+
+namespace Testing
+{
+    [TestClass]
+    class LinqTests
+    {
+        [TestMethod]
+        public void PaginateSimpleTest()
+        {
+            var intArray = Enumerable.Range(1, 100);
+            var paged = intArray.Paginate(10);
+            Assert.IsTrue(paged.All(grp => grp.Count() == 10));
+        }
+
+        [TestMethod]
+        public void PaginateUnevenTest()
+        {
+            var intArray = Enumerable.Range(1, 99);
+            var paged = intArray.Paginate(10);
+            Assert.IsTrue(paged.Where(grp => grp.Key < 10).All(grp => grp.Count() == 10));
+            Assert.IsTrue(paged.Where(grp => grp.Key == 10).All(grp => grp.Count() == 9));
+        }
+
+        [TestMethod]
+        public void PaginateTestLessThanPageSize()
+        {
+            var intArray = Enumerable.Range(1, 4);
+            var paged = intArray.Paginate(10);
+            Assert.IsTrue(paged.Count == 1);
+            Assert.IsTrue(paged.First().Count() == 4);
+        }
+
+        [TestMethod]
+        public void PartitionSimpleTest()
+        {
+            var intArray = Enumerable.Range(1, 100);
+            var partitioned = intArray.Partition(4);
+            // even distribution is the easy case
+            Assert.IsTrue(partitioned.All(p => p.Count() == 25));
+
+            // this uneven case adds one to the last partition
+            partitioned = intArray.Partition(3);
+            Assert.IsTrue(partitioned.Count() == 3);
+            Assert.IsTrue(partitioned[0].Count() == 33);
+            Assert.IsTrue(partitioned[1].Count() == 33);
+            Assert.IsTrue(partitioned[2].Count() == 34);
+
+            // this will test a really arbitrary/uneven partition
+            partitioned = intArray.Partition(7);
+            var items = partitioned[4].Select(i => i).ToArray();
+            var shouldBe = Enumerable.Range(57, 14).ToArray();
+            Assert.IsTrue(items.SequenceEqual(shouldBe));
+        }
+    }
+}
